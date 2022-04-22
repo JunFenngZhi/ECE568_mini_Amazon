7e8029b7c394bea3a7a44e47933002f6c4151725
JunFenngZhi <920019398@qq.com>
2022-04-21 23:45:29 -0400

    unzip protobuf-3.20.0

--- web-app/initserver.sh
+++ web-app/initserver.sh
@@ -3,11 +3,11 @@ python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py makemigrations amazon
 python3 manage.py migrate amazon
-res="$?"
-while [ "$res" != "0" ]
-do
-    sleep 3;
-    python3 manage.py migrate
-    res="$?"
-done
+# res="$?"
+# while [ "$res" != "0" ]
+# do
+#     sleep 3;
+#     python3 manage.py migrate
+#     res="$?"
+# done
 
